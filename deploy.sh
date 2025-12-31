9ab4746586cf973209cf37cf0a23b8c13fd403cc
mzakrzewski <michal.zakrzewski88@gmail.com>
2025-12-31 16:20:47 +0100

    remove `pnpm-lock.yaml`: cleanup unused lock file following dependency management updates

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 git pull origin main
-pnpm install
-pnpm run build
-pnpm prune --prod
+npm install
+npm run build
+npm prune --omit=dev
 composer install --no-dev --optimize-autoloader --no-interaction
 php artisan optimize
